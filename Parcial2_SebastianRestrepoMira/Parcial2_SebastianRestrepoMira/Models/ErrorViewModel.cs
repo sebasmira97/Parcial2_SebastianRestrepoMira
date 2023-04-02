0dcaff9f8a33a643a157e8fc88ac5c9cf1627f4c
Sebastian Restrepo Mira <sebarestrepo97@hotmail.com>
2023-04-01 22:17:11 -0500

    Creation of the new branch and the project

---
+++ Parcial2_SebastianRestrepoMira/Parcial2_SebastianRestrepoMira/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_SebastianRestrepoMira.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
