fcfb260475329de955bf9b72f928cfdde09a20d1
Sebastian Restrepo Mira <sebarestrepo97@hotmail.com>
2023-04-01 22:59:10 -0500

    Creation of the DatabaseContext and the new Ticket entity

---
+++ Parcial2_SebastianRestrepoMira/Parcial2_SebastianRestrepoMira/DAL/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_SebastianRestrepoMira.DAL.Entities;
+
+namespace Parcial2_SebastianRestrepoMira.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }  
+    }
+}
---
+++ Parcial2_SebastianRestrepoMira/Parcial2_SebastianRestrepoMira/DAL/Entities/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Parcial2_SebastianRestrepoMira.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Display(Name = "Uso de Boleto")]
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
+        public string? EntranceGate { get; set; }
+    }
+}
